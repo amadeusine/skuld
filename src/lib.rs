818c14d8eeb8673e1214f1e43ad6d67220bb26b9
Wesley Norris <repnop@outlook.com>
2020-03-26 17:21:40 -0400

    Add initial project structure

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! # `skuld`
+//!
+//! `skuld` is a pure-Rust asynchronous client library for the LDAP protocol. It is
+//! still very early in development so expect changes often!
+//!
+//! ## Examples
+//!
+//! TODO: when code exists
